f5b7e8bce070889b56cb8213084126c572452b9f
MrSallivan <nik.sallivan@gmail.com>
2019-10-24 23:01:44 +1000

    выполнил задание

--- JavaScript, часть 1 основы и функции/w4/index.js
+++ JavaScript, часть 1 основы и функции/w4/index.js
@@ -4,25 +4,59 @@
  * @returns {Array}
  */
 function query(collection) {
-}
-
-/**
- * @params {String[]}
- */
-function select() {
-
-}
-
-/**
- * @param {String} property – Свойство для фильтрации
- * @param {Array} values – Массив разрешённых значений
- */
-function filterIn(property, values) {
-
-}
-
-module.exports = {
+    var data = collection.slice();
+    var fns = [].slice.call(arguments).slice(1);
+  
+    if (fns.length !== 0) {
+      fns.sort().forEach(function(fn) {
+        data = fn(data);
+      })
+    }  
+  
+    return data;
+  }
+  
+  /**
+   * @params {String[]}
+   */
+  function select() {
+    var fields = [].slice.call(arguments);
+  
+    return function select(data) {
+      return data.reduce(function(arr, item) {
+        var selected = {};
+  
+        for (key in item) {
+          if (fields.indexOf(key) !== -1) {
+            selected[key] = item[key];
+          }
+        }
+  
+        arr.push(selected);
+  
+        return arr;
+      }, [])
+    }
+  }
+  
+  /**
+   * @param {String} property – Свойство для фильтрации
+   * @param {Array} values – Массив разрешённых значений
+   */
+  function filterIn(property, values) {
+    return function filter(data) {
+      return data.reduce(function(arr, item) {
+        if (values.indexOf(item[property]) !== -1) {
+          arr.push(item);
+        }
+  
+        return arr;
+      }, [])
+    }
+  }
+  
+  module.exports = {
     query: query,
     select: select,
     filterIn: filterIn
-};
+  };
